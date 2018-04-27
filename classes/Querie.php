4b11d70271ff54b1550046b85a07d0c2b6af74f7
jado <tony76.200@hotmail.fr>
2018-04-27 09:29:04 +0200

    Ecriture querie

--- classes/Querie.php
+++ classes/Querie.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dell
- * Date: 26/04/2018
- * Time: 16:55
- */
+
 
 namespace Tools;
 
@@ -17,19 +12,19 @@ class Querie
     private  $db;
 
     public function __construct()
+{
+    try
     {
-        try
-        {
-            $this->db = new PDO($this->dsn, $this->user, $this->password);
-            $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        }
-
-        catch (PDOExecption $e)
-        {
-            Log::logwrite($e->getMessage());
-        }
-
+        $this->db = new PDO($this->dsn, $this->user, $this->password);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     }
+
+    catch (PDOExecption $e)
+    {
+        Log::logwrite($e->getMessage());
+    }
+
+}
 
     public function selectMethod($sql)
     {
